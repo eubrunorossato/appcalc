95ea8b8832c5f52b955aaf828911dd80b1441762
eubrunorossato <bruno_facul@outlook.com>
2019-05-23 23:57:15 -0300

    mosquito position random

--- Game Mata Mosquito/jogo.js
+++ Game Mata Mosquito/jogo.js
@@ -11,6 +11,10 @@ alturaLargura()
 
 function posicaoMosquito(){
 
+    if(document.getElementById('mosquito')){
+        document.getElementById('mosquito').remove()
+    }
+
     var x = Math.floor(Math.random() * largura) - 90
     var y = Math.floor(Math.random() * altura) - 90
 
@@ -30,9 +34,36 @@ function posicaoMosquito(){
 
     var mosquito = document.createElement('img')
     mosquito.src='pictures/mosquito.png'
-    mosquito.className = 'mosquito1'
+    mosquito.className = randomSize() + ' ' + randomSide()
     mosquito.style.left = x + 'px';
     mosquito.style.top = y + 'px';
     mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
     document.body.appendChild(mosquito)
+    console.log(randomSide())
+
+
+}
+
+function randomSize(){
+    var classe = Math.floor(Math.random() * 3)
+    console.log(classe)
+
+    switch(classe){
+        case 0 : return 'mosquito1'
+
+        case 1 : return 'mosquito2'
+
+        case 2 : return 'mosquito3'
+    }
+
+}
+
+function randomSide(){
+    var classe = Math.floor(Math.random() * 2)
+    switch(classe){
+        case 0 : return 'ladoB'
+
+        case 1 : return 'ladoA'
+    }
 }
