de5e8f859d6ad5399e04d95160c0f716c4d1c0c4
Bruno Rossato <brunorossato@diogos-mbp-2.br.ibm.com>
2019-05-28 18:10:32 -0300

    almost finished

--- Game Mata Mosquito/jogo.js
+++ Game Mata Mosquito/jogo.js
@@ -2,6 +2,7 @@ var altura = 0;
 var largura = 0;
 var coracoesAfetados = 0;
 var teste = false;
+tempoRestante = 10;
 
 function mosquitoLoop(){
     setInterval(function() {
@@ -38,7 +39,18 @@ function randomSide(){
     }
 }
 
-function posicaoMosquito(){  
+function cronometro(){
+
+    if(tempoRestante === 0){
+        window.location.replace("voce_ganhou.html")
+    } 
+
+       tempoRestante-- 
+       document.getElementById('cronometro').innerHTML = tempoRestante
+    return tempoRestante  
+}
+
+function posicaoMosquito(){     
 
     if(document.getElementById('mosquito')){
         coracoesAfetados += 1;
@@ -88,4 +100,8 @@ function reiniciar(){
 
 function paginaInicial(){
     window.location.href = "pagina_inicial.html"
+}
+
+function comecarJogo(){
+    window.location.href = "game_page.html"
 }
