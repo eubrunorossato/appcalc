99d1523d21028ba7e378ddfb79dc3a78b2033b8f
eubrunorossato <bruno_facul@outlook.com>
2019-05-27 22:32:17 -0300

    BootStrap Added - btns createds - life bar logic created - btns logics created

--- Game Mata Mosquito/jogo.js
+++ Game Mata Mosquito/jogo.js
@@ -1,53 +1,23 @@
 var altura = 0;
 var largura = 0;
+var coracoesAfetados = 0;
+var teste = false;
+
+function mosquitoLoop(){
+    setInterval(function() {
+        posicaoMosquito() 
+    }, 3000)
+}
 
 function alturaLargura(){
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura + " " + largura)
 }
 
 alturaLargura()
 
-function posicaoMosquito(){
-
-    if(document.getElementById('mosquito')){
-        document.getElementById('mosquito').remove()
-    }
-
-    var x = Math.floor(Math.random() * largura) - 90
-    var y = Math.floor(Math.random() * altura) - 90
-
-        if(x<0){
-            x = 0
-        } else{
-            x = x
-        }
-
-        if(y<0){
-            y = 0
-        } else{
-            y = y
-        }
-
-    console.log(x + " " + y)
-
-    var mosquito = document.createElement('img')
-    mosquito.src='pictures/mosquito.png'
-    mosquito.className = randomSize() + ' ' + randomSide()
-    mosquito.style.left = x + 'px';
-    mosquito.style.top = y + 'px';
-    mosquito.style.position = 'absolute'
-    mosquito.id = 'mosquito'
-    document.body.appendChild(mosquito)
-    console.log(randomSide())
-
-
-}
-
 function randomSize(){
     var classe = Math.floor(Math.random() * 3)
-    console.log(classe)
 
     switch(classe){
         case 0 : return 'mosquito1'
@@ -66,4 +36,56 @@ function randomSide(){
 
         case 1 : return 'ladoA'
     }
+}
+
+function posicaoMosquito(){  
+
+    if(document.getElementById('mosquito')){
+        coracoesAfetados += 1;
+        document.getElementById('mosquito').remove()
+
+        if(coracoesAfetados === 3){
+            window.location.replace("game_over_page.html")
+        }else{
+            document.getElementById('v' + coracoesAfetados).src = "pictures/coracao_vazio.png"
+        }
+
+    }
+
+    var x = Math.floor(Math.random() * largura) - 90
+    var y = Math.floor(Math.random() * altura) - 90
+
+        if(x<0){
+            x = 0
+        } else{
+            x = x
+        }
+
+        if(y<0){
+            y = 0
+        } else{
+            y = y
+        }
+
+
+    var mosquito = document.createElement('img')
+    mosquito.src='pictures/mosquito.png'
+    mosquito.className = randomSize() + ' ' + randomSide()
+    mosquito.style.left = x + 'px';
+    mosquito.style.top = y + 'px';
+    mosquito.style.position = 'absolute'
+    mosquito.id = 'mosquito'
+    document.body.appendChild(mosquito)
+    mosquito.onclick=function(){
+        this.remove()
+        //mosquitoLoop()
+    }
+}
+
+function reiniciar(){
+    window.location.href = "game_page.html"
+}
+
+function paginaInicial(){
+    window.location.href = "pagina_inicial.html"
 }
